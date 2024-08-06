411bb1d91f52f1fb4b8d3b91a10008fcec1e85e3
Vishnu Hidesh <vishnuhideshnt@gmail.com>
2024-08-06 09:43:14 +0530

    done

--- ctf_game/settings.py
+++ ctf_game/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#c86ywbg-rj*shu3moke8zs25-f&#gz&-8(vz6dw_#2#y42xj2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app',]
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
